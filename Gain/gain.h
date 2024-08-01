e656352d5db00caf11e5cd9c6d18745649441d5a
Jesús Valdez <jsvaldezv@gmail.com>
2024-07-31 22:40:50 -0600

    Reduce 6dB snippet

---
+++ Gain/gain.h
@@ -0,0 +1,15 @@
+// Reducir 6dB a señal
+for (int channel = 0; channel < buffer.getNumChannels(); channel++)
+{
+    for (int i = 0; i < buffer.getNumSamples(); i++)
+    {
+        // Obtener el sample de entrada
+        float sample = buffer.getSample(channel, i);
+
+        // Reducir 6dB a sample
+        float processedSample = sample * 0.5f;
+
+        // Regresar sample para escucharlo
+        buffer.setSample(channel, i, processedSample);
+    }
+}
